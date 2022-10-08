941c1f228a4c70dcecc160c10f58259a501f6183
Devon Ryan <dpryan79@users.noreply.github.com>
2022-10-08 13:58:00 +0200

    Update build.sh

--- recipe/build.sh
+++ recipe/build.sh
@@ -1,10 +1,9 @@
 #!/bin/bash
-
+set -ex
 export DISABLE_AUTOBREW=1
 
 # shellcheck disable=SC2086
-which Rscript
 if [[ "$CONDA_BUILD_STATE" != "TEST" && "$build_platform" != "$target_platform" ]]; then
-  export Rscript=$BUILD_PREFIX/bin/Rscript
+  mv $PREFIX/bin/Rscript $PREFIX/bin/Rscript.bak
 fi
 ${R} CMD INSTALL --build . ${R_ARGS}
