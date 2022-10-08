e2311a402e3aa3fefd98770d54a34e314ac7047e
Devon Ryan <dpryan79@users.noreply.github.com>
2022-10-08 14:53:49 +0200

    Update build.sh

--- recipe/build.sh
+++ recipe/build.sh
@@ -1,10 +1,6 @@
 #!/bin/bash
-set -ex
+
 export DISABLE_AUTOBREW=1
 
 # shellcheck disable=SC2086
-if [[ "$CONDA_BUILD_STATE" != "TEST" && "$build_platform" != "$target_platform" ]]; then
-  mv $PREFIX/lib/R/bin/Rscript $PREFIX/lib/R/bin/Rscript.bak
-  ln -s $BUILD_PREFIX/lib/R/bin/Rscript $PREFIX/lib/R/bin/Rscript
-fi
 ${R} CMD INSTALL --build . ${R_ARGS}
